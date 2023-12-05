f81f51c2f6c208bfab60d54c0625536406e752a5
Paul Mana <paul@malikaz.com>
2023-12-05 03:09:33 -0700

    done dam

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -1,6 +1,8 @@
-use std::{collections::HashMap, fs::read_to_string, ops::Range, thread::sleep, time::Duration};
+use std::{
+    cmp::min, collections::HashMap, fs::read_to_string, ops::Range, thread::{sleep, JoinHandle, self}, time::Duration, sync::Arc,
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Direction {
     SeedToSoil,
     SoilToFert,
@@ -11,7 +13,7 @@ pub enum Direction {
     HumiToLoc,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SourceDestination {
     pub source_range: Vec<Range<usize>>,
     pub destination_range: Vec<Range<usize>>,
@@ -29,101 +31,47 @@ pub struct FinishedMap {
     pub location: usize,
 }
 
-pub fn solve(input: Vec<String>) -> usize {
-    let mut seeds: Vec<FinishedMap> = parse_seeds(&input);
-    let source_maps: Vec<SourceDestination> = parse_source_maps(input);
+pub fn get_lowest(seeds: &Range<usize>, source_maps: Arc<Vec<SourceDestination>>) -> usize {
+    let mut current_value: usize;
+    let mut min_value: usize = usize::MAX;
 
-    for seed in seeds.iter_mut() {
+    for seed in seeds.start..seeds.end {
         // vec should be in order
+        current_value = seed;
         for source_destination in source_maps.iter() {
-            match source_destination.direction {
-                Direction::SeedToSoil => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.seed) {
-                            seed.soil = source_destination.destination_range[index].start
-                                + (&seed.seed - source.start);
-                            break;
-                        } else {
-                            seed.soil = seed.seed;
-                        }
-                    }
-                }
-                Direction::SoilToFert => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.soil) {
-                            seed.fetilizier = source_destination.destination_range[index].start
-                                + (&seed.soil - source.start);
-                            break;
-                        } else {
-                            seed.fetilizier = seed.soil;
-                        }
-                    }
-                }
-                Direction::FertToWater => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.fetilizier) {
-                            seed.water = source_destination.destination_range[index].start
-                                + (&seed.fetilizier - source.start);
-                            break;
-                        } else {
-                            seed.water = seed.fetilizier;
-                        }
-                    }
-                }
-                Direction::WaterToLight => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.water) {
-                            seed.light = source_destination.destination_range[index].start
-                                + (&seed.water - source.start);
-                            break;
-                        } else {
-                            seed.light = seed.water;
-                        }
-                    }
-                }
-                Direction::LightToTemp => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.light) {
-                            seed.temperature = source_destination.destination_range[index].start
-                                + (&seed.light - source.start);
-                            break;
-                        } else {
-                            seed.temperature = seed.light;
-                        }
-                    }
-                }
-                Direction::TempToHumi => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.temperature) {
-                            seed.humidity = source_destination.destination_range[index].start
-                                + (&seed.temperature - source.start);
-                            break;
-                        } else {
-                            seed.humidity = seed.temperature;
-                        }
-                    }
-                }
-                Direction::HumiToLoc => {
-                    for (index, source) in source_destination.source_range.iter().enumerate() {
-                        if source.contains(&seed.humidity) {
-                            seed.location = source_destination.destination_range[index].start
-                                + (&seed.humidity - source.start);
-                            break;
-                        } else {
-                            seed.location = seed.humidity;
-                        }
-                    }
+            for (index, source) in source_destination.source_range.iter().enumerate() {
+                if source.contains(&current_value) {
+                    current_value =
+                        source_destination.destination_range[index].start + (current_value - source.start);
+                    break;
+                } else {
+                    current_value = current_value;
                 }
             }
         }
+        min_value = min(min_value, current_value);
     }
-    *seeds
-        .iter()
-        .map(|e| e.location)
-        .collect::<Vec<usize>>()
-        .iter_mut()
-        .min()
-        .unwrap()
+    min_value
+}
+
+pub fn solve_two(input: Vec<String>) -> usize {
+    let seeds = Vec::leak(parse_seeds(&input[0]));
+    let source_maps = Arc::new(parse_source_maps(input));
+    let mut lowest: usize = usize::MAX;
+    let mut handles: Vec<JoinHandle<usize>> = Vec::new();
+
+
+    for seed in seeds.iter() {
+        let source_clone = source_maps.clone();
+        handles.push(thread::spawn(move || {
+            println!("Starting need range {:?}", seed);
+            get_lowest(seed, source_clone)
+        }));
+    }
+    for handle in handles.into_iter(){
+        lowest = min(handle.join().unwrap(), lowest);
+    }
+    lowest
 }
 
 pub fn get_direction(input: &str) -> Direction {
@@ -179,31 +127,17 @@ pub fn parse_source_maps(input: Vec<String>) -> Vec<SourceDestination> {
     source_maps
 }
 
-pub fn parse_seeds(input: &Vec<String>) -> Vec<FinishedMap> {
-    let mut seeds: Vec<FinishedMap> = Vec::new();
-    // seeds:
-    let input = input[0].replace("seeds: ", "");
+pub fn parse_seeds(input: &String) -> Vec<Range<usize>> {
+    let input = input.replace("seeds: ", "");
     let seed_range = input.split(" ").collect::<Vec<&str>>();
-
-    for item in seed_range.windows(2).step_by(2) {
-        println!("Making seeds from{:?} to {:?}", item[0], item[1]);
-        // soooooooo fuck
-        let start = item[0].parse::<usize>().unwrap();
-        let offset = item[1].parse::<usize>().unwrap() + start;
-        for number in start..offset {
-            seeds.push(FinishedMap {
-                seed: number,
-                soil: 0,
-                fetilizier: 0,
-                temperature: 0,
-                humidity: 0,
-                water: 0,
-                location: 0,
-                light: 0,
-            });
-        }
+    let mut seed_ranges: Vec<Range<usize>> = Vec::new();
+    
+    for seed in seed_range.windows(2).step_by(2) {
+        let start = seed[0].parse::<usize>().unwrap();
+        let end = start + seed[1].parse::<usize>().unwrap();
+        seed_ranges.push(start..end);
     }
-    seeds
+    seed_ranges
 }
 
 pub fn read_lines(filename: &str) -> Vec<String> {
@@ -222,7 +156,7 @@ mod tests {
 
     #[test]
     fn day_05_it_works() {
-        let result = solve(read_lines("src/test_input"));
+        let result = solve_two(read_lines("src/test_input"));
         assert_eq!(result, 46);
     }
 }
--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,4 +1,4 @@
 use day_05;
 pub fn main(){
-    println!("{:}?", day_05::solve(day_05::read_lines("day_05/src/input")));
+    println!("{:}?", day_05::solve_two(day_05::read_lines("./input")));
 }
